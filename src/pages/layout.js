10f37a5d118714b1b39a07d4168ece3e7fce1f53
Skyler_Hawkins <sjh6425@psu.edu>
2025-04-08 12:28:58 -0400

    removing metadata

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,10 +1,6 @@
 // app/layout.js
 import '/styles/globals.css';
 
-export const metadata = {
-  title: 'Skyler Hawkins Portfolio',
-  description: 'Portfolio website for Skyler Hawkins',
-};
 
 export default function RootLayout({ children }) {
   return (
