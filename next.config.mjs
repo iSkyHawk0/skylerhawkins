7941c4bb5c8414dcceb22abdf74cc2b6d34b599e
Skyler_Hawkins <sjh6425@psu.edu>
2025-04-08 12:43:09 -0400

    fixed assetPathing and basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  basePath: isProd ? 'skylerhawkins' : '',
+  assetPrefix: isProd? 'skylerhawkins' : '',
+  images: {
+    unoptimized: true,
+  },
   compiler: {
     styledComponents: true, // Move this here
   },
